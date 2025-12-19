ba89fc6a33e1ddc39cfc3b64bfddd52e7a2d25df
Utkarsh1506 <utkarsh.choudhary_cs.csf22@gla.ac.in>
2025-12-19 15:04:05 +0530

    Clean up: Remove obsolete HTML, documentation, and setup files for Next.js migration

--- agnivridhi-site/js/config.js
+++
@@ -1,6 +0,0 @@
-// Deprecated: config.js is no longer used. URL is hardcoded in js/main.js.
-// This file remains only to avoid confusion if referenced accidentally.
-// If you re-enable it, ensure HTML includes <script src="js/config.js"></script> before main.js.
-(function(){
-  window.AGNI_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbxhXssWcCEYQarFtOakmxU4iizigxPbPUiPf-_-Bb368ZFpB45GEw0lrKpXYgdk9Ix3/exec';
-})();
